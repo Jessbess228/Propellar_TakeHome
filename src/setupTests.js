412b55386fe6f5db2ef7aea95751ca081bc2b70f
Jessica Berry <berryjessica416@gmail.com>
2025-06-09 17:14:13 +1000

    Tests added

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,26 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders zoom buttons', () => {
+  render(<App />);
+  expect(screen.getByText('+')).toBeInTheDocument();
+  expect(screen.getByText('-')).toBeInTheDocument();
+});
+
+test('renders at least one tile', () => {
+  render(<App />);
+  const tiles = screen.getAllByRole('img');
+  expect(tiles.length).toBeGreaterThan(0);
+});
+
+test('zooming in increases the number of tiles', () => {
+  render(<App />);
+  const plusButton = screen.getByText('+');
+  const initialTiles = screen.getAllByRole('img').length;
+  plusButton.click();
+  const moreTiles = screen.getAllByRole('img').length;
+  expect(moreTiles).toBeGreaterThan(initialTiles);
+});
